513abfa665f3e07b556ce98d34f1502d20e63674
Malharkai <shotdragon96@hotmail.com>
2022-04-19 12:54:41 +0200

    Add files via upload

---
+++ DBLab3/DBLab3/Db/MusicTracksDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DBLab3.Db
+{
+    public partial class MusicTracksDbContext : DbContext
+    {
+        public MusicTracksDbContext()
+        {
+        }
+
+        public MusicTracksDbContext(DbContextOptions<MusicTracksDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Track> Tracks { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-891DTUT;Initial Catalog=everyloop;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Finnish_Swedish_CI_AS");
+
+            modelBuilder.Entity<Track>(entity =>
+            {
+                entity.ToTable("tracks", "music");
+
+                entity.Property(e => e.TrackId).ValueGeneratedNever();
+
+                entity.Property(e => e.Composer).HasMaxLength(220);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DBLab3/DBLab3/Db/Track.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBLab3.Db
+{
+    public partial class Track
+    {
+        public int TrackId { get; set; }
+        public string Name { get; set; } = null!;
+        public int? AlbumId { get; set; }
+        public int MediaTypeId { get; set; }
+        public int? GenreId { get; set; }
+        public string? Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
